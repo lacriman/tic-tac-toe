a7b05f1ec1bb594f068699f16e2e8b0b6acb7e56
Yaroslav <proskurin.yarik@gmail.com>
2025-05-21 20:35:02 +0200

    refactor(game): begin replacing hardcoded win checks with loop-based logic (horizontal only)

--- game/rules.go
+++ game/rules.go
@@ -1,17 +1,28 @@
 package game
 
 func CheckWin(board [3][3]string, turn string) bool {
-			// vertical lines
-	return ((board[0][0] == turn && board[1][0] == turn && board[2][0] == turn) ||
-			(board[0][1] == turn && board[1][1] == turn && board[2][1] == turn) ||
-			(board[0][2] == turn && board[1][2] == turn && board[2][2] == turn) ||
-
-			// horizontal lines
-			(board[0][0] == turn && board[0][1] == turn && board[0][2] == turn) ||
-			(board[1][0] == turn && board[1][1] == turn && board[1][2] == turn) ||
-			(board[2][0] == turn && board[2][1] == turn && board[2][2] == turn) ||
-
-			// slash lines
-			(board[0][0] == turn && board[1][1] == turn && board[2][2] == turn) ||
-			(board[0][2] == turn && board[1][1] == turn && board[2][0] == turn))
+	for i := 0; i < 3; {
+		if board[i][0] == turn && board[i][1] == turn && board[i][2] == turn {
+			return true
+		} else {
+			i++
+		}
+	}
+	return false
 }
+
+// func CheckWin(board [3][3]string, turn string) bool {
+// 			// vertical lines
+// 	return ((board[0][0] == turn && board[1][0] == turn && board[2][0] == turn) ||
+// 			(board[0][1] == turn && board[1][1] == turn && board[2][1] == turn) ||
+// 			(board[0][2] == turn && board[1][2] == turn && board[2][2] == turn) ||
+
+// 			// horizontal lines
+// 			(board[0][0] == turn && board[0][1] == turn && board[0][2] == turn) ||
+// 			(board[1][0] == turn && board[1][1] == turn && board[1][2] == turn) ||
+// 			(board[2][0] == turn && board[2][1] == turn && board[2][2] == turn) ||
+
+// 			// slash lines
+// 			(board[0][0] == turn && board[1][1] == turn && board[2][2] == turn) ||
+// 			(board[0][2] == turn && board[1][1] == turn && board[2][0] == turn))
+// }
